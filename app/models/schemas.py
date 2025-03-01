7e3334c989b0f049e49eb964f9c13eae3dfabf1e
Магомед Гаджиев <mgadziev@MacBook-Pro-Magomed.local>
2025-03-02 02:44:18 +0300

    model changes

--- app/models/schemas.py
+++ app/models/schemas.py
@@ -1,14 +1,28 @@
 from pydantic import BaseModel
+from typing import Optional
 
 # Модели для обработки данных
 class MessageContent(BaseModel):
-    text: str = None
+    text: Optional[str] = None
 
-class Message(BaseModel):
-    author_id: int
+class Value(BaseModel):
+    id: str
     chat_id: str
+    user_id: int
+    author_id: int
+    created: int
+    type: str
     chat_type: str
     content: MessageContent
-    id: str
-    item_id: int = None
+    item_id: Optional[int] = None
+    published_at: str
+
+class Payload(BaseModel):
     type: str
+    value: Value
+
+class WebhookRequest(BaseModel):
+    id: str
+    version: str
+    timestamp: int
+    payload: Payload
--- app/routes/chat.py
+++ app/routes/chat.py
@@ -1,10 +1,10 @@
 from fastapi import APIRouter
-from app.models.schemas import Message
+from app.models.schemas import Value
 from app.services.gpt import process_message
 
 router = APIRouter()
 
 @router.post("/chat/")
-async def chat(message: Message):
+async def chat(message: Value):
     response = process_message(message.author_id, message.content.text)
     return {"response": response}
