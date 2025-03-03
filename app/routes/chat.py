94b72088f2533275f201dccaf575351c87ce9d3c
Магомед Гаджиев <mgadziev@MacBook-Pro-Magomed.local>
2025-03-03 14:03:42 +0300

    fix

--- app/routes/chat.py
+++ app/routes/chat.py
@@ -1,6 +1,4 @@
-import time
-
-from fastapi import APIRouter
+from fastapi import APIRouter, BackgroundTasks
 from app.models.schemas import WebhookRequest
 from app.services.avito_api import send_message
 from app.services.gpt import process_message
@@ -10,7 +8,7 @@ router = APIRouter()
 
 
 @router.post("/chat")
-async def chat(message: WebhookRequest):
+async def chat(message: WebhookRequest, background_tasks: BackgroundTasks):
     # Автор последнего сообщения
     if message.payload.value.author_id == int(75107414):
         return None
@@ -18,10 +16,9 @@ async def chat(message: WebhookRequest):
         # Генерация ответа на сообщение пользователя
         response = process_message(message.payload.value.author_id, message.payload.value.content.text)
 
+        # Добавляем задачу на выполнение в фоне
+        background_tasks.add_task(send_message, message.payload.value.user_id, message.payload.value.chat_id,
+                                  response)
+
         # Возвращаем ответ с кодом 200
-        result = {"response": response}
-
-        # Отправка сгенерированного сообщения после формирования ответа
-        await send_message(message.payload.value.user_id, message.payload.value.chat_id, response)
-
-        return JSONResponse(content=result, status_code=200)
+        return JSONResponse(content={"response": response}, status_code=200)
