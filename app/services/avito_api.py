6683304e21eab2a95f742803af2b693132ac186e
Магомед Гаджиев <mgadziev@MacBook-Pro-Magomed.local>
2025-03-03 14:59:33 +0300

    fix

--- app/services/avito_api.py
+++ app/services/avito_api.py
@@ -55,4 +55,4 @@ def send_message(user_id: int, chat_id: str, text: str):
         print(f"Ошибка при отправке сообщения: {response.status_code}, {response.text}")
         return None
 
-    return JSONResponse(content={"response": "ok"}, status_code=200)
+    return None
