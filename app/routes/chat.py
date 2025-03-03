576c4265f856c9637eb8258ee825f104bebdd8ee
Магомед Гаджиев <mgadziev@MacBook-Pro-Magomed.local>
2025-03-03 14:55:35 +0300

    fix

--- app/routes/chat.py
+++ app/routes/chat.py
@@ -12,16 +12,8 @@ def chat(message: WebhookRequest):
     if message.payload.value.author_id ==int(75107414):
         print('0. Вебхук на сообщение от самого себя')
         return JSONResponse(content={"response": "ok"}, status_code=200)
-    send_message(message.payload.value.user_id, message.payload.value.chat_id, "Тестовый ответ")
-
-    # Автор последнего сообщения
-    # if message.payload.value.author_id ==int(75107414):
-    #     print('0. Вебхук на сообщение от самого себя')
-    #     return JSONResponse(content={"response": "ok"}, status_code=200)
-    # else:
-    #     print('1. Генерация ответа на сообщение пользователя')
-    #     response = process_message(message.payload.value.author_id, message.payload.value.content.text)
-    #     print('2. Отправка сгенерированного сообщения')
-    #     send_message(message.payload.value.user_id, message.payload.value.chat_id, response)
-    #     return JSONResponse(content={"response": "ok"}, status_code=200)
+    print('1. Генерация ответа на сообщение пользователя')
+    response = process_message(message.payload.value.author_id, message.payload.value.content.text)
+    print('2. Отправка сгенерированного сообщения')
+    send_message(message.payload.value.user_id, message.payload.value.chat_id, response)
     return JSONResponse(content={"response": "ok"}, status_code=200)
