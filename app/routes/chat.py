44d3804ef1cd8b3e743fa7b422a7715b3ae8f6be
Магомед Гаджиев <mgadziev@MacBook-Pro-Magomed.local>
2025-03-03 14:31:06 +0300

    fix

--- app/routes/chat.py
+++ app/routes/chat.py
@@ -10,12 +10,13 @@ router = APIRouter()
 async def chat(message: WebhookRequest):
     print('ПОЛУЧЕН НОВЫЙ ЗАПРОС ОТ АВИТО')
     # Автор последнего сообщения
-    if message.payload.value.author_id ==int(75107414):
-        print('0. Вебхук на сообщение от самого себя')
-        return JSONResponse(content={"response": "ok"}, status_code=200)
-    else:
-        print('1. Генерация ответа на сообщение пользователя')
-        response = process_message(message.payload.value.author_id, message.payload.value.content.text)
-        print('2. Отправка сгенерированного сообщения')
-        send_message(message.payload.value.user_id, message.payload.value.chat_id, response)
-        return JSONResponse(content={"response": "ok"}, status_code=200)
+    # if message.payload.value.author_id ==int(75107414):
+    #     print('0. Вебхук на сообщение от самого себя')
+    #     return JSONResponse(content={"response": "ok"}, status_code=200)
+    # else:
+    #     print('1. Генерация ответа на сообщение пользователя')
+    #     response = process_message(message.payload.value.author_id, message.payload.value.content.text)
+    #     print('2. Отправка сгенерированного сообщения')
+    #     send_message(message.payload.value.user_id, message.payload.value.chat_id, response)
+    #     return JSONResponse(content={"response": "ok"}, status_code=200)
+    return None
