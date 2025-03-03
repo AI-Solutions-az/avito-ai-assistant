84aec819499273fd25a3fcd659de37f6ca1c80b0
Магомед Гаджиев <mgadziev@MacBook-Pro-Magomed.local>
2025-03-03 12:04:41 +0300

    fix

--- app/routes/chat.py
+++ app/routes/chat.py
@@ -10,12 +10,12 @@ router = APIRouter()
 @router.post("/chat")
 async def chat(message: WebhookRequest):
     # Автор последнего сообщения
-    print("Автор последнего сообщения:", message.payload.value.author_id)
+    print("Автор последнего сообщения:", message.payload.value.author_id, "Сообщение:", message.payload.value.content.text)
     # Генерация ответа на сообщение пользователя
     if message.payload.value.author_id !=75107414:
-        response = process_message(message.payload.value.author_id, message.payload.value.content.text)
+        # response = process_message(message.payload.value.author_id, message.payload.value.content.text)
         # Отправка сгенерированного сообщения
-        send_message(message.payload.value.user_id, message.payload.value.chat_id, response)
-        return {"response": response}
+        # send_message(message.payload.value.user_id, message.payload.value.chat_id, response)
+        return None # {"response": response}
     else:
         return None
