58fc9571aa71936ddf4b187532fbe603facfc874
Магомед Гаджиев <mgadziev@MacBook-Pro-Magomed.local>
2025-03-03 10:54:47 +0300

    fix

--- app/routes/chat.py
+++ app/routes/chat.py
@@ -7,13 +7,13 @@ from app.services.gpt import process_message
 
 router = APIRouter()
 
-@router.post("/chat/")
+@router.post("/chat")
 async def chat(message: WebhookRequest):
     # Автор последнего сообщения
     print(message.payload.value.author_id)
     time.sleep(3)  # Таймер, чтобы успело отправиться предыдущее сообщение
     # Генерация ответа на сообщение пользователя
-    if message.payload.value.author_id != '75107414':
+    if message.payload.value.author_id !=75107414:
         response = process_message(message.payload.value.author_id, message.payload.value.content.text)
         # Отправка сгенерированного сообщения
         send_message(message.payload.value.user_id, message.payload.value.chat_id, response)
