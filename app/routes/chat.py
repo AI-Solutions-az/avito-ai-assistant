83041ffd5a1cdbe2de07647710c3cb28a2a12132
Магомед Гаджиев <mgadziev@MacBook-Pro-Magomed.local>
2025-03-05 11:13:35 +0300

    fix

--- app/routes/chat.py
+++ app/routes/chat.py
@@ -13,7 +13,6 @@ logger = logging.getLogger("uvicorn")
 
 router = APIRouter()
 
-
 # Вынесение джобы в отдельную функцию, чтобы работало как надо
 def process_and_send_response(message: WebhookRequest):
     logger.info('1. Генерация ответа на сообщение пользователя')
@@ -32,17 +31,27 @@ def process_and_send_response(message: WebhookRequest):
         logger.info("4.2. Добавление чата в список исключений")
         add_chat(message.payload.value.chat_id)
 
-
 @router.post("/chat")
 def chat(message: WebhookRequest, background_tasks: BackgroundTasks):
     logger.info('ПОЛУЧЕН НОВЫЙ ЗАПРОС ОТ АВИТО')
+    print(message)
+    author_id = int(75107414)
+    message_text = message.payload.value.content.text
+    chat_id = message.payload.value.chat_id
 
-    if message.payload.value.author_id == int(75107414):
-        logger.info('0. Вебхук на сообщение от самого себя')
+    if chat_exists(chat_id):
+        logger.info('0. Ассистент отключен в чате')
         return JSONResponse(content={"ok": True}, status_code=200)
 
-    if chat_exists(message.payload.value.chat_id):
-        logger.info('0. Ассистент отключен в чате')
+    if message.payload.value.author_id == author_id:
+        if (re.search('оператор', message_text, re.IGNORECASE) or
+                re.search('менеджер', message_text, re.IGNORECASE)):
+            logger.info("4.3. Переключение на оператора самим оператором или чат-ботом")
+            send_alert(f"Требуется внимание менеджера:\n ссылка")
+            logger.info("4.4. Добавление чата в список исключений")
+            add_chat(chat_id)
+        else:
+            logger.info('0. Вебхук на сообщение от самого себя')
         return JSONResponse(content={"ok": True}, status_code=200)
 
     # Добавляем выполнение кода в фоне
