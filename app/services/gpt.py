8f810cf067d700b629032978c617d7885f255157
Магомед Гаджиев <mgadziev@MacBook-Pro-Magomed.local>
2025-03-27 05:40:24 +0300

    added return tool

--- app/services/gpt.py
+++ app/services/gpt.py
@@ -1,10 +1,11 @@
 import logging
 from openai import OpenAI
-import os
+import os, json
 from app.redis_db import save_message, get_history
 from dotenv import load_dotenv
 from app.config import prompt
 from app.services.google_sheets_api import fetch_google_sheet_stock, get_knowledge_base
+from app.services.telegram_bot import send_alert
 
 load_dotenv()
 client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
@@ -15,20 +16,24 @@ logger = logging.getLogger("uvicorn")
 # Генерация ответа на сообщение клиента
 def process_message(user_id: str, chat_id:str, message: str, ad_url):
     logger.info(f"3.1. Получено сообщение от пользователя {user_id}: {message}")
-
     logger.info("3.2. Получение информации по объявлению из базы знаний")
     stock = fetch_google_sheet_stock(ad_url)
+
     # Выключаем бота, если не нашли объявление в базе знаний и отправляем уведомление об этом
     if not stock:
         # send_alert(f'Невозможно найти объявление {ad_url} в базе знаний')
         logger.warning("3.2.1. Объявление не найдено в базе знаний")
         return None
+
     # Запрос базы знаний
     logger.info("3.3. Получение информации по кейсам из базы знаний")
     knowledge_base = get_knowledge_base()
-    # Получение истории сообщений
+
+    # Добавление сообщения от пользователя в историю
+    save_message(user_id, chat_id, "user", message)
+
+    # Получение истории сообщений вместе с новым сообщением
     history = get_history(user_id, chat_id)
-    history.append({"role": "user", "content": message})
     # Инструкция бота
     instructions = {"role": "developer", "content": f"{prompt}\n"
                                                     f"# INFORMATION: {stock}"
@@ -38,12 +43,68 @@ def process_message(user_id: str, chat_id:str, message: str, ad_url):
     messages = [instructions] + history
 
     logger.info("3.4. Отправка запроса в ChatGPT")
-    response = client.chat.completions.create(model="gpt-4o-mini", messages=messages)
+    response = client.chat.completions.create(model="gpt-4o-mini"
+                                              , messages=messages
+                                              , tools=[
+                                                {
+                                                  "type": "function",
+                                                  "function": {
+                                                    "name": "initiate_return",
+                                                    "description": "Get reason of return and order date",
+                                                    "parameters": {
+                                                      "type": "object",
+                                                      "properties": {
+                                                        "date_of_order": {
+                                                          "type": "string",
+                                                          "description": "Date when order is done"
+                                                        },
+                                                        "reason": {
+                                                          "type": "string",
+                                                          "description": "Reason why client want to return the good"
+                                                        }
+                                                      },
+                                                      "required": ["date_of_order", "reason"]
+                                                    }
+                                                  }
+                                                }]
+                                              , tool_choice="auto"
+                                              )
+
+    finish_reason = response.choices[0].finish_reason
+    # Проверка был ли вызов инструмента
+    if finish_reason=='tool_calls':
+        logger.info("3.5. Произошел вызов инструмента")
+        # Достаем tool_calls
+        tool_call = response.choices[0].message.tool_calls[0]
+        # Определяем какая функция была вызвана
+        name = tool_call.function.name
+
+        if name == 'initiate_return':
+            logger.info("3.6. Произошел вызов инструмента ВОЗВРАТА")
+            # Парсим JSON аргументы
+            arguments = json.loads(tool_call.function.arguments)
+            # Извлекаем нужные поля
+            date_of_order = arguments.get("date_of_order")
+            reason = arguments.get("reason")
+
+            # Оформление возврата и оповещение менеджера
+            send_alert(f"Новая заявка на возврат\n\n"
+                       f"Заказ от: {date_of_order}\n"
+                       f"Причина: {reason}")
+
+            # Генерация ответа пользователю
+            instructions = [{"role": "developer"
+                                , "content": f'Сообщи о том, что возврат оформлен на заказа от {date_of_order} по причине {reason}'
+                             }]
+            response = client.chat.completions.create(model="gpt-4o-mini", messages=instructions)
+            reply = response.choices[0].message.content
+            logger.info("3.7. Сохранение истории в редис")
+            save_message(user_id, chat_id, "developer", reply)
+            return reply
 
     reply = response.choices[0].message.content
-
-    logger.info("3.5. Сохранение истории в редис")
-    save_message(user_id, chat_id, "user", message)
+    logger.info("3.8. Сохранение истории в редис")
     save_message(user_id, chat_id, "developer", reply)
 
+    # Ответ если не было вызова инструмента
     return reply
--- app/services/telegram_bot.py
+++ app/services/telegram_bot.py
@@ -1,14 +1,12 @@
 import logging
 import requests
 import os
-
+from app.config import TELEGRAM_BOT_TOKEN, TELEGRAM_CHAT_ID
 # Настройка логирования
 logger = logging.getLogger("uvicorn")
 
-TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
-TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
 
-
+# Отправка сообщения в чат
 def send_alert(message):
     logger.info(f"Отправка уведомления в Telegram: {message}")
 
@@ -22,3 +20,25 @@ def send_alert(message):
     except requests.exceptions.RequestException as e:
         logger.error(f"Ошибка при отправке уведомления в Telegram: {e}")
         raise
+
+
+def create_telegram_forum_topic(topic_name):
+    url = f'https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/createForumTopic'
+
+    # Параметры для создания топика
+    params = {
+        'chat_id': TELEGRAM_CHAT_ID,
+        'name': topic_name,
+        'title': '12345'
+    }
+
+    # Отправка запроса
+    response = requests.post(url, data=params)
+
+    # Проверка ответа
+    if response.status_code == 200:
+        print(f"Топик '{topic_name}' успешно создан!")
+    else:
+        print(f"Ошибка при создании топика: {response.text}")
+
+
